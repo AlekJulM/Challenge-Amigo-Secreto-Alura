046d13638a178174c0f641742ca07ccfc91c9b50
Alexander Jamin Julon Mayta <alexanderjjm01@gmail.com>
2025-02-13 21:12:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+let amigos = [];
+
+function agregarAmigo() {
+  const inputAmigo = document.getElementById("amigo");
+  let nombre = inputAmigo.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  if (amigos.includes(nombre)) {
+    return;
+  }
+
+  amigos.push(nombre);
+
+  inputAmigo.value = "";
+
+  actualizarLista();
+}
+
+
+function actualizarLista() {
+  const listaAmigos = document.getElementById("listaAmigos");
+  
+  listaAmigos.innerHTML = "";
+  
+  amigos.forEach(amigo => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+  });
+}
+
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("La lista de amigos está vacía, por favor añade nombres.");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  const amigoSecreto = amigos[indiceAleatorio];
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>Tu amigo secreto es: ${amigoSecreto}</li>`;
+}
+
+
+function resetearSorteo() {
+ 
+  amigos = [];
+  
+  actualizarLista();
+  
+  document.getElementById("resultado").innerHTML = "";
+}
+
+
+const inputAmigo = document.getElementById("amigo");
+inputAmigo.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    agregarAmigo();
+  }
+});
